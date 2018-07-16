0254722448b5a455a84404dd84bbfb525946e41f
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2018-07-16 17:28:44 -0300

    Fix: log colors.

--- back/project/main.js
+++ back/project/main.js
@@ -8,7 +8,7 @@ const base = require('./models/base')
  * Run the challenge with fabulous logs
  */
 async function run(){
-    console.log('\033[95m----- Initializating Database -----')
+    console.log('\033[95m----- Initializating Database -----\033[0m')
     // Set up default mongoose connection
     var db_string = 'mongodb://127.0.0.1/blog'
     mongoose.connect(db_string)
@@ -19,7 +19,7 @@ async function run(){
     // Initializate models
     base.init(mongoose)
     
-    console.log('\033[95m----- Inserting data to mongo -----')
+    console.log('\033[95m----- Inserting data to mongo -----\033[0m')
     
     for(let m = 0; m < base.models.length; m++){
         model = base.models[m];
@@ -38,11 +38,11 @@ async function run(){
         });
 
         await base[model].create(obj)
-        console.log('\033[94m----- Inserted ' + model + ' -----')
+        console.log('\033[94m----- Inserted ' + model + ' -----\033[0m')
     }
     
 
-    console.log('\033[95m----- Starting Chalenge -----')
+    console.log('\033[95m----- Starting Chalenge -----\033[0m')
     const start = new Date()
 
     const list = require('./controllers/list_users_by_reaction')
@@ -51,27 +51,27 @@ async function run(){
     // Starting Tests and printg "OK" or "OH NO"
     let compareTest = function(users, file_number){
         if(JSON.stringify(users) == fs.readFileSync(`./sample/out/${file_number}.json`, 'utf8')){
-            console.log('\033[92mOK')
+            console.log('\033[92mOK\033[0m')
         }else{
-            console.log('\033[94mOH NO')
+            console.log('\033[94mOH NO\033[0m')
         }
     }
 
-    console.log('\033[95m----- Test 1 -----')
+    console.log('\033[95m----- Test 1 -----\033[0m')
     users = await list(base, 'ab', 'love')
     compareTest(users, 1)
 
-    console.log('\033[95m----- Test 2 -----')
+    console.log('\033[95m----- Test 2 -----\033[0m')
     users = await list(base, 'ac', 'hate')
     compareTest(users, 2)
 
-    console.log('\033[95m----- Test 3 -----')
+    console.log('\033[95m----- Test 3 -----\033[0m')
     users = await list(base, '', 'like')
     compareTest(users, 3)
 
-    console.log('\033[95m----- Finishing Chalenge -----')
+    console.log('\033[95m----- Finishing Chalenge -----\033[0m')
     const end = new Date()
-    console.log('\033[94mTotal time [ms]: ' + (end - start))
+    console.log('\033[94mTotal time [ms]: ' + (end - start) + '\033[0m')
 
     process.exit()
 }
